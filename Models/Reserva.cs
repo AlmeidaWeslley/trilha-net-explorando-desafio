5b4bc81946e6cb28abc130861120b8d03a7bb8de
Weslley Almeida <Weslley.almeida.f@gmail.com>
2025-09-30 17:45:27 -0300

    Desafio Concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTADO
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +26,8 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                // IMPLEMENTADO
+                throw new Exception("A quantidade de hospedes é maior que a suite suporta!");
             }
         }
 
@@ -37,7 +40,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            // IMPLEMENTADO
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +49,15 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // IMPLEMENTADO
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENADO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.90M;
             }
 
             return valor;
